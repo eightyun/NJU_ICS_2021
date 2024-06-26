d5a8bfed2cef6434dbd8ea4e8f0fa47fd56de847
eightyun <879950144@qq.com>
2024-06-26 17:45:33 +0800

    定义isa_diffest_checkregs函数

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -2,8 +2,22 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
-bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+// 进行寄存器状态的对比
+bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc)
+{
+  int reg_num = ARRLEN(cpu.gpr);
+    for (int i = 0; i < reg_num; i++) 
+    {
+      if (ref_r->gpr[i]._64 != cpu.gpr[i]._64) 
+      {
+        return false;
+      }
+    }
+    if (ref_r->pc != cpu.pc) 
+    {
+      return false;
+    }
+  return true;
 }
 
 void isa_difftest_attach() {
