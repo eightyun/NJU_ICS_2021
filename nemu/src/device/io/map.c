621ed44050c04b7451b738fbf408954939426986
eightyun <879950144@qq.com>
2024-06-27 19:42:24 +0800

    新增注释

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -8,6 +8,7 @@
 static uint8_t *io_space = NULL;
 static uint8_t *p_space = NULL;
 
+// I/O空间的分配
 uint8_t* new_space(int size) {
   uint8_t *p = p_space;
   // page aligned;
@@ -37,12 +38,15 @@ void init_map() {
   p_space = io_space;
 }
 
+//map_read()和map_write()用于将地址addr映射到map所指示的目标空间, 并进行访问.
+
 word_t map_read(paddr_t addr, int len, IOMap *map) {
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
+  trace_dread(addr, len, map);
   return ret;
 }
 
@@ -52,4 +56,5 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
+  trace_dwrite(addr, len, data, map);
 }
