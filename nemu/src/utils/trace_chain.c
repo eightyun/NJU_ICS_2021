b7d773e4fe3bb759a246db2e356500d7d956032c
eightyun <879950144@qq.com>
2024-06-27 19:41:37 +0800

    新增设备访问踪迹函数

--- nemu/src/utils/trace_chain.c
+++ nemu/src/utils/trace_chain.c
@@ -612,3 +612,19 @@ void trace_func_ret(paddr_t pc)
 		}
 	}
 }
+
+//------------------------------------dtrace----------------------------------------------
+
+// 记录设备访问踪迹
+
+void trace_dread(paddr_t addr, int len, IOMap *map) 
+{
+	dtrace_write("dtrace: read %10s at " FMT_PADDR ",%d\n",
+		map->name, addr, len);
+}
+
+void trace_dwrite(paddr_t addr, int len, word_t data, IOMap *map) 
+{
+	dtrace_write("dtrace: write %10s at " FMT_PADDR ",%d with " FMT_WORD "\n",
+		map->name, addr, len, data);
+}
