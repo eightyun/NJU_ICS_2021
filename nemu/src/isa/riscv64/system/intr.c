bfee0b80ce2adc0c642f387a8a744e1f5cd48f56
eightyun <879950144@qq.com>
2024-07-07 01:09:35 +0800

    完成了isa_raise

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -1,11 +1,15 @@
 #include <isa.h>
 
-word_t isa_raise_intr(word_t NO, vaddr_t epc) {
+word_t isa_raise_intr(word_t NO, vaddr_t epc) 
+{
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-
-  return 0;
+  disable_intr();
+  csr(CSR_MEPC) = epc;
+  csr(CSR_MCAUSE) = NO;
+  
+  return csr(CSR_MTVEC);
 }
 
 word_t isa_query_intr() {
